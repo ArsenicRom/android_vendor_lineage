znl/android_vendor_havoc

Add Pixel Launcher

config/common.mk
@@ -114,7 +114,13 @@ endif
 
 # Havoc Packages
 PRODUCT_PACKAGES += \
+    PixelLauncher \
     SoundPickerPrebuilt
+
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/etc/sysconfig/google-hiddenapi-package-whitelist.xml:system/etc/sysconfig/google-hiddenapi-package-whitelist.xml \
+    vendor/havoc/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
 
 # Exchange support
 PRODUCT_PACKAGES += \

ArsenicRom/android_vendor_lineage

config/common.mk
@@ -172,7 +172,13 @@ PRODUCT_PACKAGES += \
 
 # Arsenic Packages
 PRODUCT_PACKAGES += \
+    PixelLauncher \
     SoundPickerPrebuilt
+
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/google-hiddenapi-package-whitelist.xml:system/etc/sysconfig/google-hiddenapi-package-whitelist.xml \
+    vendor/lineage/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
 
 # Exchange support
 PRODUCT_PACKAGES += \