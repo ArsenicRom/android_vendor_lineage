Havoc-OS/android_vendor_havoc

Cleanup lockscreen weather implementation [2/2]

config/common.mk
@@ -137,15 +137,12 @@ PRODUCT_PACKAGES += \
     Terminal \
     Turbo \
     WallpaperPickerGooglePrebuilt \
-    WeatherClient
+    WeatherProvider
 
-# Weather
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.havoc.weather.revision=2
-
+# WeatherProvider
 PRODUCT_COPY_FILES += \
-    vendor/havoc/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
-    vendor/havoc/prebuilt/common/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
+    vendor/havoc/prebuilt/common/etc/permissions/com.android.providers.weather.xml:system/etc/permissions/com.android.providers.weather.xml \
+    vendor/havoc/prebuilt/common/etc/default-permissions/com.android.providers.weather.xml:system/etc/default-permissions/com.android.providers.weather.xml
 
 # Ambient Play
 PRODUCT_PACKAGES += \

ArsenicRom/android_vendor_lineage

config/common.mk
@@ -202,10 +202,12 @@ PRODUCT_PACKAGES += \
 # Weather
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.weather.revision=2
+    WeatherProvider
 
+# WeatherProvider
 PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
-    vendor/lineage/prebuilt/common/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
+    vendor/lineage/prebuilt/common/etc/permissions/com.android.providers.weather.xml:system/etc/permissions/com.android.providers.weather.xml \
+    vendor/lineage/prebuilt/common/etc/default-permissions/com.android.providers.weather.xml:system/etc/default-permissions/com.android.providers.weather.xml
 
 # Substratum Key
 PRODUCT_COPY_FILES += \