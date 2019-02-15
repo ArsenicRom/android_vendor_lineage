Havoc-OS/android_vendor_havoc

Weather: Add default permissions
Change-Id: Icba08d4b98452b9d57c771e1719fb002c7019cf5

config/common.mk
@@ -144,7 +144,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.havoc.weather.revision=2
 
 PRODUCT_COPY_FILES += \
-    vendor/havoc/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
+    vendor/havoc/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/havoc/prebuilt/common/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
 
 # Ambient Play
 PRODUCT_PACKAGES += \

ArsenicRom/android_vendor_lineage

config/common.mk
@@ -204,7 +204,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.weather.revision=2
 
 PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
+    vendor/lineage/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/lineage/prebuilt/common/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
 
 # Substratum Key
 PRODUCT_COPY_FILES += \