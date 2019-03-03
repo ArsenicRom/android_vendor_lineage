Havoc-OS/android_vendor_havoc

vendor: Cleanup

config/telephony.mk
@@ -1,10 +1,18 @@
-# Sensitive Phone Numbers list
-PRODUCT_COPY_FILES += \
-    vendor/havoc/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
-
 # World APN list
 PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \

ArsenicRom/android_vendor_lineage

config/telephony.mk
@@ -1,10 +1,18 @@
-# Sensitive Phone Numbers list
-PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
-
 # World APN list
 PRODUCT_PACKAGES += \
     apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \